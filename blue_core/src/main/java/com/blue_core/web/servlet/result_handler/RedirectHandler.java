6232ab034545a9c6b763c36977d89d94a4e70e74
Jason <18479385003@163.com>
2023-02-17 14:47:20 +0800

    feat(result_handler): 新增重定向、请求转发结果处理器

--- blue_core/src/main/java/com/blue_core/web/servlet/result_handler/RedirectHandler.java
+++ blue_core/src/main/java/com/blue_core/web/servlet/result_handler/RedirectHandler.java
@@ -1,9 +1,39 @@
 package com.blue_core.web.servlet.result_handler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @Author Jason
  * @CreationDate 2023/02/10 - 17:10
- * @Description ：TODO 重定向处理器
+ * @Description ：重定向处理器，当处理器返回的结果为 redirect: 开头的String类的话，就会重定向到其后面的uri中
  */
-public class RedirectHandler {
+public class RedirectHandler extends AbstractResultHandler{
+    public static final String REDIRECT_URI_PREFIX = "redirect:";
+
+    @Override
+    public boolean support(Object result) {
+        return result instanceof String && ((String) result).trim().startsWith(REDIRECT_URI_PREFIX);
+    }
+
+    @Override
+    public int getPriority() {
+        return -1;
+    }
+
+    @Override
+    protected String getContentType() {
+        return null;
+    }
+
+    @Override
+    public void outputResult(Object result, HttpServletRequest request, HttpServletResponse response) {
+        String uri = ((String)result).trim().substring(REDIRECT_URI_PREFIX.length());
+        try {
+            response.sendRedirect(request.getContextPath()+uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
